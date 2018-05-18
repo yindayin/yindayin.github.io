9d20b390d20052cb4df2eae963d1fe65aa42f559
yindayin <yindayin@gmail.com>
2018-05-18 15:26:49 -0400

    Update trigger for buttons in main-content and add contact info

--- js/main.js
+++ js/main.js
@@ -24,6 +24,14 @@
 		}, 600);
 	});
 
+	$(".home-wrapper .home-content a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
 	$('#back-to-top').on('click', function(){
 		$('body,html').animate({
 			scrollTop: 0
